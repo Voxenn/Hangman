3fb8822e79508d53d8858ba8c446cd0c75c9fc5a
Vernon Williams <voxenn@gmail.com>
2018-08-02 19:21:32 -0500

    Add logic to place multiple characters into string.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,9 @@ var wordBank = ['spike', 'faye', 'ed', 'swordfish'];
 var currentWord = wordBank[Math.floor((Math.random() * wordBank.length))];
 var wordLength = '';
 var guessLeft = 5;
-var guessTaken = 0;
+var guessTaken = '';
+var found = 0;
+var inputArr = [];
 
 function main() {
   $('#guessNumLeft').text(guessLeft);
@@ -12,29 +14,51 @@ function main() {
   }
   $('#wordContainer').text(wordLength);
   $(document).on("keypress", function(e) {
+    //Get the keystroke the user entered
     let input = String.fromCharCode(e.which);
-    let guess = searchString(input);
-    let guessPos = wordLength.charAt(guess);
-    if((typeof guess === "undefined") && guessLeft != 0) {
-      guessTaken++;
-      guessLeft--;
-      $(".guess-button").text("Try again!");
-      $('#guessNumLeft').text(guessLeft);
-      $('#guessNumTake').text(guessTaken);
-    } else if (guess === -1 && guessLeft === 0) {
-      $(".guess-button").text("Game...over.");
+    //Search for character setting an array of where the character was found in the word
+    var guess = searchString(input);
+    //Depending on the answer, we'll either loop through to fill in all spots, set the single char, or set guess to undefined
+    if (found > 1) {
+      for (var i = 0; i < found ; i++) {
+          guessChar(input, inputArr[i]);
+      }
+    } else if ( found === 1) {
+          guessChar(input, inputArr[0]);
     } else {
-      wordLength = wordLength.substring(0, guess) + input + wordLength.substring(guess + 1);
-      $('#wordContainer').text(wordLength);
-      $(".guess-button").text("Keep going!");
+       guess = null;
     }
   });
 }
+
+//Function to search the current word for a single character
 function searchString(input) {
+  found = 0;
+  inputArr = [];
   for (var i = 0; i < currentWord.length; i++) {
       if(currentWord.charAt(i) === input) {
-          return i;
+          found++;
+          inputArr.push(i);
       }
+  }
+}
+
+//Function to guess the user's input
+function guessChar(userGuess, guess) {
+  this.guess = guess;
+  this.input = userGuess;
+  if((typeof this.guess === null) && guessLeft != 0) {
+    guessTaken++;
+    guessLeft--;
+    $(".guess-button").text("Try again!");
+    $('#guessNumLeft').text(guessLeft);
+    $('#guessNumTake').append(this.input);
+  } else if (this.guess === null && guessLeft === 0) {
+    $(".guess-button").text("Game...over.");
+  } else {
+    wordLength = wordLength.substring(0, this.guess) + this.input + wordLength.substring(this.guess + 1);
+    $('#wordContainer').text(wordLength);
+    $(".guess-button").text("Keep going!");
   }
 }
 $(document).ready(main);
