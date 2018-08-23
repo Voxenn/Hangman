80b9281d21f165fc499e1a2850ad7de511661e26
Vernon Williams <voxenn@gmail.com>
2018-08-22 20:15:32 -0500

    Fix styles. Restart game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var wordBank = ['spike', 'faye', 'edward', 'swordfish'];
 var currentWord = wordBank[Math.floor((Math.random() * wordBank.length))];
-var hint = 'assets/images/' + currentWord + '.jpg';
+var hint = '';
 var wordLength = '';
 var guessLeft = 5;
 var guessTaken = '';
@@ -10,27 +10,13 @@ var inputArr = [];
 function main() {
   $('#guessNumLeft').text(guessLeft);
   $('#guessNumTake').text(guessTaken);
+  hint = 'assets/images/' + currentWord + '.jpg';
   $('img').attr("src", hint);
   for (let i = 0; i < currentWord.length ; i++) {
       wordLength = wordLength.concat('-');
   }
   $('#wordContainer').text(wordLength);
-  $(document).on("keypress", function(e) {
-    //Get the keystroke the user entered
-    let input = String.fromCharCode(e.which);
-    //Search for character setting an array of where the character was found in the word
-    let guess = searchString(input);
-    //Depending on the answer, we'll either loop through to fill in all spots, set the single char, or set guess to undefined
-    if (found > 1) {
-      for (var i = 0; i < found ; i++) {
-          guessChar(input, inputArr[i]);
-      }
-    } else if ( found === 1) {
-          guessChar(input, inputArr[0]);
-    } else {
-          guessChar(input, null);
-    }
-  });
+  userGuessFunc();
 }
 
 //Function to search the current word for a single character
@@ -59,7 +45,8 @@ function guessChar(userGuess, guess) {
   } else if (this.guess === null && guessLeft === 1) {
     guessLeft--;
     $('#guessNumLeft').text(guessLeft);
-    $(".guess-button").text("Game...over.");
+    $(".guess-button").text("Game...over. Restarting in 3 seconds!");
+    setTimeout(resetGame, 3000);
   } else {
     wordLength = wordLength.substring(0, this.guess) + this.input + wordLength.substring(this.guess + 1);
     $('#wordContainer').text(wordLength);
@@ -68,5 +55,27 @@ function guessChar(userGuess, guess) {
   if (currentWord === wordLength) {
     $(".guess-button").text("You win!");
   }
+}
+
+function userGuessFunc(){
+  $(document).on("keypress", function(e) {
+    //Get the keystroke the user entered
+    let input = String.fromCharCode(e.which);
+    //Search for character setting an array of where the character was found in the word
+    let guess = searchString(input);
+    //Depending on the answer, we'll either loop through to fill in all spots, set the single char, or set guess to undefined
+    if (found > 1) {
+      for (var i = 0; i < found ; i++) {
+          guessChar(input, inputArr[i]);
+      }
+    } else if ( found === 1) {
+          guessChar(input, inputArr[0]);
+    } else {
+          guessChar(input, null);
+    }
+  });
+}
+function resetGame(){
+  location.reload();
 }
 $(document).ready(main);
